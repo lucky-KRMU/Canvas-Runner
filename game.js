5feaf1773097147d107ea0c7832acab53bb526cc
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-24 09:26:02 +0000

    add player move logic

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
 console.log("test!")
+
+let player = document.querySelector("#player");
+let body = document.querySelector("body");
+
+let playerInitialPos = 2;   // To get the initial position of the player and get and update the current value of the player
+
+body.addEventListener("keydown", (e) => {
+    // ArrowRight, ArrowLeft, ArrowUp, ArrowDown
+
+    //Moving the element sideways
+    //Also checking for the valid requested moves via checking initial position
+    if (e.key === "ArrowLeft" && playerInitialPos > 1) {
+
+        playerInitialPos -= 1;
+        player.style.gridColumn = playerInitialPos;
+        console.log(player.style.gridColumn)
+
+    } else if (e.key === "ArrowRight" && playerInitialPos < 3) {
+
+        playerInitialPos += 1;
+        player.style.gridColumn = playerInitialPos;
+        console.log(player.style.gridColumn)
+        
+    }
+})
