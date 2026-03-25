6764708e3532ea90a54431907739847775f22ddd
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-25 17:50:13 +0000

    add collision logic

--- logic.js
+++ logic.js
@@ -5,13 +5,20 @@ let body = document.querySelector("body")
 let board = document.querySelector("#container");
 let player = document.querySelector("#player");
 
+// Obstacle Variables (data structure)
 let obstacleArray = [];
 let obstaclePositionArray = [];
 
+// auxiliary variables
 let lane = 1;
 let laneLeft = 15;
-
 let jumpTime = 200;
+let gameOver = false;
+
+
+// Defining the Initial position of the player to let js read and compare it's values
+player.style.top = "80%";
+player.style.left = "15%";
 
 // Player Mechanics
 body.addEventListener('keydown', (e)=>{
@@ -71,3 +78,65 @@ function moveObstacles() {
         obstacle.style.top = `${position}%`
     })
 }
+
+// Removing the obstacles After leaving the board
+function removeObstacles() {
+    obstacleArray.forEach((obstacle, index)=>{
+        if (obstacle.style.top === "90%") {
+            board.removeChild(obstacle);
+            obstacleArray.splice(index, 1);
+            obstaclePositionArray.splice(index, 1);
+        }
+    })
+}
+
+// Function to detect collisions
+function detectCollision() {
+    obstacleArray.forEach(obstacle=>{
+        // Method 1
+        // let obsX = Math.round(obstacle.getBoundingClientRect().left);
+        // let obsY = Math.round(obstacle.getBoundingClientRect().top);
+
+        // let playerX = Math.round(player.getBoundingClientRect().left);
+        // let playerY = Math.round(player.getBoundingClientRect().top);
+
+        // if(obsX == playerX || obsY >= playerY){
+        //     console.log("collision!");
+        // }
+
+        // Method 2
+        let obsX = obstacle.style.left;
+        let obsY = obstacle.style.top;
+        let obsZ = obstacle.style.transform || "";
+
+        let playerX = player.style.left;
+        let playerY = player.style.top;
+        let playerZ = player.style.transform;
+
+        if (obsX == playerX && obsY >= playerY && obsZ == playerZ){
+            gameOver = true;
+            alert("Game Over");
+            window.location.reload();
+        }
+
+        
+        // The problem with the collision was that we were using Method 2, It used CSS properties for collision. But JS didn't read the CSS properties from CSS file initially. hence we gave that manually in JS. Now it's working
+    })
+}
+
+// Game Loop
+setInterval(()=>{
+
+    // Adding obstacles
+    makeObstacles();
+
+    // Moving obstacles
+    moveObstacles();
+
+    // Removing obstacles after leaving the board
+    removeObstacles();
+
+    // Detecting Collisions
+    detectCollision();
+
+}, 1000);
