b3ce9c9655c02dbd2bc85e57b1efdca7e12c061d
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-25 16:51:41 +0000

    add player jump logic

--- logic.js
+++ logic.js
@@ -8,17 +8,31 @@ let player = document.querySelector("#player");
 let lane = 1;
 let laneLeft = 15;
 
+let jumpTime = 200;
+
 // Player Mechanics
 body.addEventListener('keydown', (e)=>{
     
     // Changing the Lane
     if (e.key === "ArrowRight" && lane < 3){
+        // Changing/Updating the lane, left of the player and it's position
         lane += 1;
         laneLeft += 30;
         player.style.left = `${laneLeft}%`;
     } else if (e.key === "ArrowLeft" && lane > 1){
+        // Changing/Updating the lane, left of the player and it's position
         lane -= 1;
         laneLeft -= 30;
         player.style.left = `${laneLeft}%`;
+    }
+
+    if (e.key === "ArrowUp"){
+        // Making the player jump 
+        player.style.transform = "translateZ(10vh)";
+        
+        // Making the player return to it's original position
+        setTimeout(()=>{
+            player.style.transform = "";
+        }, jumpTime)
     }
 })
