cb3d74a39f84ffd6832e51c8ff74041efad29cdd
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-25 16:48:39 +0000

    add lane change logic

--- logic.js
+++ logic.js
@@ -1,5 +1,24 @@
 // This is the new logic.js file. This file will be used to redesign the game mechanics
 
 // Necessary Objects (variables)
+let body = document.querySelector("body")
 let board = document.querySelector("#container");
 let player = document.querySelector("#player");
+
+let lane = 1;
+let laneLeft = 15;
+
+// Player Mechanics
+body.addEventListener('keydown', (e)=>{
+    
+    // Changing the Lane
+    if (e.key === "ArrowRight" && lane < 3){
+        lane += 1;
+        laneLeft += 30;
+        player.style.left = `${laneLeft}%`;
+    } else if (e.key === "ArrowLeft" && lane > 1){
+        lane -= 1;
+        laneLeft -= 30;
+        player.style.left = `${laneLeft}%`;
+    }
+})
