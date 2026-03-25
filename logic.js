bcc646ba948b2351475b1b9015481ab8a4a1c5ee
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-25 15:46:45 +0530

    add new rel. pos mechanics

---
+++ logic.js
@@ -0,0 +1,5 @@
+// This is the new logic.js file. This file will be used to redesign the game mechanics
+
+// Necessary Objects (variables)
+let board = document.querySelector("#container");
+let player = document.querySelector("#player");
